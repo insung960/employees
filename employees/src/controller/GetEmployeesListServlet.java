9a1d63d38a8be05f8f6d4ce18c34594ecf1c30cb
insun <insun@DESKTOP-J23EET7>
2019-09-22 14:43:00 +0900

    employees 테이블 mvc 패턴으로 구현 		{ 			controller안의 GetEmployeesListServlet 			model안의 EmployeesDao 			vo안의 Employees 			WEB-INF/views/employees/employeesList.jsp 		} 		를 추가함

--- employees/src/controller/GetEmployeesListServlet.java
+++ employees/src/controller/GetEmployeesListServlet.java
@@ -10,22 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.DepartmentsDao;
+import model.EmployeesDao;
 import vo.Departments;
+import vo.Employees;
 
-@WebServlet("/departments/getDepartmentsList")			//주소 표시
-public class GetDepartmentsListServlet extends HttpServlet 
+@WebServlet("/GetEmployeesListServlet")
+public class GetEmployeesListServlet extends HttpServlet 
 {
-	private DepartmentsDao departmentsDao;	//departmentsDao를 프라이빗으로 먼저 만든다
+	private EmployeesDao employeesDao;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{		
-		departmentsDao = new DepartmentsDao();
+	{
+		employeesDao = new EmployeesDao();
 		//list를 만들어 departmentsDao안의 selectDepartmentsList메서드를 실행시켜 리턴값(리스트)를 받아온다
-		List<Departments> list = departmentsDao.selectDepartmentsList();
+		List<Employees> list = employeesDao.selectEmployeesList();
 		
 		//attribute에 저장한다 이름"list" 값 list
 		request.setAttribute("list",list);
 		
 		//setAttribute로 저장한 값들을 getRequestDispatcher로 여러 페이지에 넘겨줄수있다
-		request.getRequestDispatcher("/WEB-INF/views/departments/departmentsList.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/employees/employeesList.jsp").forward(request, response);
 	}
 }
--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -4,12 +4,65 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import vo.Departments;
 import vo.Employees;
 
 public class EmployeesDao
 {
+	public List<Employees> selectEmployeesList()
+	{
+		List<Employees> list  = new ArrayList<Employees>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT emp_no,first_name,last_name,birth_date,hire_date,gender FROM employees";
+		Employees employees = new Employees();
+		try 
+		{	//널값을 다채우고 돌린다
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3306/employees","root","java123");
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while(rs.next())
+			{//rs로 반복돌려서 리스트 애드한다
+				employees = new Employees();
+				employees.setEmpNo(rs.getInt("emp_no"));
+				employees.setFirstName(rs.getString("first_name"));
+				employees.setLastName(rs.getString("last_name"));
+				employees.setGender(rs.getString("gender"));
+				employees.setBirthDate(rs.getString("birth_date"));
+				employees.setHireDate(rs.getString("hire_date"));
+				list.add(employees);				
+			}
+			System.out.println("departmentsDao의 While문 다음");
+			for(Employees e : list)
+			{
+				System.out.println(e.getEmpNo());
+			}
+		}
+		catch(Exception e)
+		{//위의 코드의 예외를 받는다
+			e.printStackTrace();
+		}
+		finally
+		{	
+			try
+			{	//무조건 한번 실행되서 모두닫는다 순서대로 닫아야함
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+			catch(Exception e)
+			{	//close의 예외용 답는다
+				e.printStackTrace();
+			}
+		}
+		
+		return list;		//만든 리스트를 Dao를 부른 서블렛에 리턴한다  >>>GetEmployeesListServlet
+	}
 	public int selectEmployeesCount()
 	{
 		int count = 0;
